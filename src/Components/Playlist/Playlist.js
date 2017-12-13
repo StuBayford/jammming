3f9957dcf4e9b7b1dcca42b22d277ded7993850e
Stuart Bayford <stu@stuartbayford.co.uk>
2017-12-13 16:48:07 +0000

    Add Playlist Component HTML

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Playlist.css';
+import Tracklist from '../Tracklist/Tracklist';
+
+class Playlist extends React.Component {
+	render() {
+		<div className="Playlist">
+		  <input defaultValue={'New Playlist'} />
+		  <!-- Add a TrackList component -->
+		  <a className="Playlist-save">SAVE TO SPOTIFY</a>
+		</div>
+	}
+}
