a94f99a8d3df5676328b64f900bb7218518f4dc3
Stuart Bayford <stu@stuartbayford.co.uk>
2017-12-13 17:16:02 +0000

    Add renderAction() to Track Component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+	renderAction() {
+		if (this.props.isRemoval) {
+			<a className="Track-action" onClick={this.removeTrack}>
+				-
+			</a>
+		} else {
+			<a className="Track-action" onClick={this.addTrack}>
+				+
+			</a>
+		}
+	}
+
 	render() {
-		<div class="Track">
-		  <div class="Track-information">
+		<div className="Track">
+		  <div className="Track-information">
 		    <h3><!-- track name will go here --></h3>
 		    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
 		  </div>
-		  <a class="Track-action"><!-- + or - will go here --></a>
+		  {this.renderAction()}
 		</div>
 	}
 }
+
+export default Track;
