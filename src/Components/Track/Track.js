0df5b9afd40779f96ec6e7ea8dd8c4df58e0bb81
Stuart Bayford <stu@stuartbayford.co.uk>
2017-12-13 16:58:47 +0000

    Add Track Component HTML

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+	render() {
+		<div class="Track">
+		  <div class="Track-information">
+		    <h3><!-- track name will go here --></h3>
+		    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+		  </div>
+		  <a class="Track-action"><!-- + or - will go here --></a>
+		</div>
+	}
+}
